5c84baa1cc2748da43cf59a217b0680449784b0c
Arkadiy <mefrreex@gmail.com>
2025-07-24 23:01:11 +0400

    refactor: remove test custom block registering from InternalPlugin

--- src/main/java/cn/nukkit/plugin/InternalPlugin.java
+++ src/main/java/cn/nukkit/plugin/InternalPlugin.java
@@ -1,37 +1,11 @@
 package cn.nukkit.plugin;
 
-import cn.nukkit.block.custom.CustomBlockDefinition;
-import cn.nukkit.block.custom.CustomBlockManager;
-import cn.nukkit.block.custom.container.CustomBlock;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class InternalPlugin extends PluginBase {
     public static final InternalPlugin INSTANCE = new InternalPlugin();
-
-    @Override
-    public void onLoad() {
-        final CustomBlock block = new CustomBlock("swp:ruby_block", CustomBlockManager.LOWEST_CUSTOM_BLOCK_ID);
-        CustomBlockManager.get().registerCustomBlock("swp:ruby_block", CustomBlockManager.LOWEST_CUSTOM_BLOCK_ID,
-                CustomBlockDefinition.builder(block)
-                        .name("Я ебал эту хуйню")
-                        .breakTime(0.1)
-                        .texture("stone")
-                        .build(), () -> {
-                    return block;
-                });
-
-        final CustomBlock block1 = new CustomBlock("swp:test", CustomBlockManager.LOWEST_CUSTOM_BLOCK_ID + 1);
-        CustomBlockManager.get().registerCustomBlock("swp:test", CustomBlockManager.LOWEST_CUSTOM_BLOCK_ID + 1,
-                CustomBlockDefinition.builder(block1)
-                        .name("Я ебал эту хуйню 2 ")
-                        .breakTime(0.1)
-                        .texture("dirt")
-                        .build(), () -> {
-                    return block1;
-                });
-    }
 
     @Override
     public void onDisable() {
