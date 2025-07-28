8ec50f1fea2bf6ba3fe6b145414f518b80dc06ac
Arkadiy <mefrreex@gmail.com>
2025-07-28 17:43:24 +0400

    refactor: remove test custom blocks

--- src/main/java/cn/nukkit/plugin/InternalPlugin.java
+++ src/main/java/cn/nukkit/plugin/InternalPlugin.java
@@ -1,65 +1,11 @@
 package cn.nukkit.plugin;
 
-import cn.nukkit.block.Block;
-import cn.nukkit.block.customblock.CustomBlockDefinition;
-import cn.nukkit.block.customblock.CustomBlock;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class InternalPlugin extends PluginBase {
     public static final InternalPlugin INSTANCE = new InternalPlugin();
-
-    private static class TestBlock extends Block implements CustomBlock {
-        @Override
-        public String getName() {
-            return "test";
-        }
-
-        @Override
-        public int getId() {
-            return CustomBlock.super.getId();
-        }
-
-        @Override
-        public CustomBlockDefinition getDefinition() {
-            return CustomBlockDefinition.builder(this, "stone").build();
-        }
-
-        @Override
-        public String getIdentifier() {
-            return "test:test";
-        }
-    }
-
-    private static class TestBlock2 extends Block implements CustomBlock {
-        @Override
-        public String getName() {
-            return "test2";
-        }
-
-        @Override
-        public int getId() {
-            return CustomBlock.super.getId();
-        }
-
-        @Override
-        public CustomBlockDefinition getDefinition() {
-            return CustomBlockDefinition.builder(this, "planks").build();
-        }
-
-        @Override
-        public String getIdentifier() {
-            return "test:goida";
-        }
-    }
-
-    @Override
-    public void onLoad() {
-        Block.registerCustomBlock(List.of(TestBlock.class, TestBlock2.class));
-    }
 
     @Override
     public void onDisable() {
