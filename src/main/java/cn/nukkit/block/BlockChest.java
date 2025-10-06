7a95cb55deb48f01bbaa9562896aff355947963f
Koshak_Mine <52908036+KoshakMineDEV@users.noreply.github.com>
2025-10-06 15:59:27 +0300

    fix: chest pairing

--- src/main/java/cn/nukkit/block/BlockChest.java
+++ src/main/java/cn/nukkit/block/BlockChest.java
@@ -125,11 +125,6 @@ public class BlockChest extends BlockTransparentMeta implements Faceable, BlockE
         }
 
         for (BlockFace side : BlockFace.Plane.HORIZONTAL) {
-            if ((this.getDamage() == 4 || this.getDamage() == 5) && (side == BlockFace.WEST || side == BlockFace.EAST)) {
-                continue;
-            } else if ((this.getDamage() == 3 || this.getDamage() == 2) && (side == BlockFace.NORTH || side == BlockFace.SOUTH)) {
-                continue;
-            }
             Block c = this.getSide(side);
             if (c instanceof BlockChest && c.getDamage() == this.getDamage()) {
                 BlockEntity entity = this.getLevel().getBlockEntity(c);
