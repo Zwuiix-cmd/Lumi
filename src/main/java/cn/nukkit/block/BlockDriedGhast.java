bdf0232485d33c7be580814ce061ad69e936916e
Koshak_Mine <52908036+KoshakMineDEV@users.noreply.github.com>
2025-11-03 21:19:17 +0300

    fix: dried ghast item

--- src/main/java/cn/nukkit/block/BlockDriedGhast.java
+++ src/main/java/cn/nukkit/block/BlockDriedGhast.java
@@ -3,8 +3,11 @@ package cn.nukkit.block;
 import cn.nukkit.Player;
 import cn.nukkit.block.data.Faceable;
 import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemBlock;
 import cn.nukkit.math.BlockFace;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class BlockDriedGhast extends BlockTransparentMeta implements Faceable {
     private static final int CARDINAL_DIRECTION_MASK = 0b0000_0000_0000_0011;
     private static final int REHYDRATION_LEVEL = 0b0000_0000_0000_1100;
@@ -28,10 +31,25 @@ public class BlockDriedGhast extends BlockTransparentMeta implements Faceable {
     }
 
     @Override
+    public double getHardness() {
+        return 0;
+    }
+
+    @Override
+    public double getResistance() {
+        return 0;
+    }
+
+    @Override
     public boolean place(Item item, Block block, Block target, BlockFace face, double fx, double fy, double fz, Player player) {
         this.setBlockFace(player.getHorizontalFacing().getOpposite());
         this.setRehydrationLevel(0);
         return this.getLevel().setBlock(this, this, true);
+    }
+
+    @Override
+    public Item toItem() {
+        return new ItemBlock(this, 0);
     }
 
     private void setProperty(int mask, int value) {
