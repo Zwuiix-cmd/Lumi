ff0697f66fe0726e4c88984c3af37fb8b3dc94a3
Reider745 <70357814+Reider745@users.noreply.github.com>
2025-11-15 14:20:09 +0300

    feat: vanilla regeneration and poison

--- src/main/java/cn/nukkit/entity/effect/EffectRegeneration.java
+++ src/main/java/cn/nukkit/entity/effect/EffectRegeneration.java
@@ -1,6 +1,8 @@
 package cn.nukkit.entity.effect;
 
 import cn.nukkit.entity.Entity;
+import cn.nukkit.entity.EntitySmite;
+import cn.nukkit.event.entity.EntityDamageEvent;
 import cn.nukkit.event.entity.EntityRegainHealthEvent;
 
 import java.awt.*;
@@ -20,8 +22,14 @@ public class EffectRegeneration extends Effect {
 
     @Override
     public void onApply(Entity entity, double tickCount) {
-        if (entity.getHealth() < entity.getMaxHealth()) {
-            entity.heal(new EntityRegainHealthEvent(entity, 1, EntityRegainHealthEvent.CAUSE_MAGIC));
+        if(entity instanceof EntitySmite) {
+            if (entity.getHealth() > 1) {
+                entity.attack(new EntityDamageEvent(entity, EntityDamageEvent.DamageCause.MAGIC, 1));
+            }
+        } else {
+            if (entity.getHealth() < entity.getMaxHealth()) {
+                entity.heal(new EntityRegainHealthEvent(entity, 1, EntityRegainHealthEvent.CAUSE_MAGIC));
+            }
         }
     }
 }
